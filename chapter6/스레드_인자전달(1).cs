cfe785d4175b754de3c91190a4805700a9afffa4
hyojin park <candy020406@naver.com>
2022-11-21 17:17:30 +0900

    6장 내용 복습

---
+++ chapter6/스레드_인자전달(1).cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Threading;
+//namespace Thread_parameter_passing_1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Thread t = new Thread(threadFunc); //스레드로 처리할 메소드 이름
+//            t.Start(10); // 스레드를 시작하는 측에서 인자를 전달
+//        }
+//        static void threadFunc(object initialValue) //object는 모든 자료형 수신 가능
+//        {
+//            int intValue = (int)initialValue; // 다운캐스팅
+//            Console.WriteLine(intValue); //다운캐스팅한 값을 출력 = 10 
+//        }
+//    }
+//}
---
+++ chapter6/스레드_인자전달(2).cs
@@ -0,0 +1,26 @@
+﻿//using System; // 스레드를 시작하는 측에서 여러개의 인자를 전달하는 방법
+//using System.Threading;
+//namespace Thread_parameter_passing_2
+//{
+//    class ThreadParam // 전달할 수만큼 필드를 포함한 클래스
+//    {
+//        public int Value1;
+//        public int Value2;
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Thread t = new Thread(threadFunc); //스레드 객체 생성
+//            ThreadParam param = new ThreadParam(); //여러 객체를 담을 객체 생성
+//            param.Value1 = 10; //값을 담음
+//            param.Value2 = 20;
+//            t.Start(param); // start() 메소드 인자에 값을 담은 객체를 전달
+//        }
+//        static void threadFunc(object initialValue) //업캐스팅
+//        {
+//            ThreadParam value = (ThreadParam)initialValue; // 다운 캐스팅
+//            Console.WriteLine("{0}, {1}", value.Value1, value.Value2);
+//        }
+//    }
+//}
